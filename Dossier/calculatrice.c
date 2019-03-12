01a64d6d88f384f28b1d4484831ca195370d8433
corentinbernier <corentin.bernier97@gmail.com>
2019-03-12 18:01:17 +0100

    Ajout de calculatrice.c

---
+++ Dossier/calculatrice.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	char res = 'o';	
+
+	do{
+	int a, b;
+	char nb;
+
+    	printf("Entrez la première valeur :");
+    	scanf("%d", &a);
+	printf("Entrez la deuxième valeur :");
+    	scanf("%d", &b);
+	printf("Entrez l'opérateur (+ - * /) :");
+    	scanf(" %c", &nb);
+
+	switch(nb){
+		case '+': printf("Résultat:\n%d %c %d = %d\n", a, nb, b, a+b);
+		break;
+		case '-': printf("Résultat:\n%d %c %d = %d\n", a, nb, b, a-b);
+		break;
+		case '/': printf("Résultat:\n%d %c %d = %d\n", a, nb, b, a/b);
+		break;
+		case '*': printf("Résultat:\n%d %c %d = %d\n", a, nb, b, a*b);
+		break;
+		default:
+		printf("Erreur: Opérateur non existant");
+	}
+	printf("Voulez vous continuer (o ou n)");
+    	scanf(" %c", &res);
+
+	}while(res != 'n');
+
+	return EXIT_SUCCESS;
+}
